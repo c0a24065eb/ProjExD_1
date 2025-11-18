94e7b88eab4951b999e8e3204360221d306bb418
Kasiwongsuntorn Chin <c0a24065eb@edu.teu.ac.jp>
2025-11-18 17:12:24 +0900

    練習１０：こうかとんを動かせる

--- pygame_image.py
+++ pygame_image.py
@@ -15,14 +15,27 @@ def main():
     bg_progress_f = 1600
     koukaton_img = pg.image.load("fig/3.png")
     koukaton_img = pg.transform.flip(koukaton_img, True, False)
+    koukaton_rect = koukaton_img.get_rect()
+    koukaton_rect.center = 300, 200
 
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+
+        key_1st = pg.key.get_pressed()
+        if key_1st[pg.K_UP]:
+            koukaton_rect.move_ip((0, -1))
+        if key_1st[pg.K_DOWN]:
+            koukaton_rect.move_ip((0, 1))
+        if key_1st[pg.K_LEFT]:
+            koukaton_rect.move_ip((-1, 0))
+        if key_1st[pg.K_RIGHT]:
+            koukaton_rect.move_ip((1, 0))
+        
         screen.blit(bg_img, [bg_progress, 0])
         screen.blit(bg_img_flipped, [bg_progress_f, 0])
-        screen.blit(koukaton_img, [300, 200])
+        screen.blit(koukaton_img, koukaton_rect)
         pg.display.update()
         tmr += 1        
         bg_progress -= 1
